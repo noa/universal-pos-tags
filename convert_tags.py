0e2a867a79f9e890a86be0a77209a863dfe5f3af
Nicholas Andrews <noandrews@gmail.com>
2015-10-05 11:15:31 -0400

    add conversion script for conll

---
+++ convert_tags.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+import argparse
+from universal_tags import *
+
+parser = ArgumentParser()
+parser.add_argument('input')
+parser.add_argument('output')
+parser.add_argument('tagset') # e.g. 'en-ptb'
+parser.add_argument('--tag-col', type=int, default=3)
+parser.add_argument('--num-col', type=int, default=10)
+parser.add_argument('--delim', default='\t')
+args = parser.parse_args()
+
+with open(args.output, 'w') as ouf:
+    with open(args.input) as f:
+        for line in f:
+            tokens = line.rstrip().split()
+            if len(tokens) == args.num_col:
+                tag = tokens[args.tag_col]
+                uni_tag = convert(tagset, tag)
+                tokens[args.tag_col] = uni_tag
+                ouf.write(args.delim.join(tokens)+'\n')
+            else:
+                ouf.write(line)
+
+# eof
